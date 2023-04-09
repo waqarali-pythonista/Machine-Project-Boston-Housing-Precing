10b51e3fb24e3b3a61489e0cfd2a6f7e86ca76ae
WaqarAli2050 <waqarsoftware310@gmail.com>
2023-04-09 16:33:12 +0500

    This commit for that Front End is Completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle 
-from flask import Flask , request , app, jsonify,url_for, render_template
+from flask import Flask,request,app, jsonify,url_for,render_template
 import numpy as np 
 from gevent.pywsgi import WSGIServer
 
@@ -13,7 +13,7 @@ regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
-def home ():
+def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
@@ -27,6 +27,17 @@ def predict_api():
     output=regmodel.predict(new_Data)
     print(output[0])
     return jsonify (output[0])
+
+@app.route('/predict',methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text=" The House Price  Prediction is  {} ".format(output))
+
+
+
 
 if __name__=="__main__":
        app.run(debug=True) 
